0dce51b8a264842624f4822eac5f9e8060a60ada
Marione Fiszman <marionef1985@gmail.com>
2023-02-08 18:27:11 +0000

    limpieza de codigo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//write your code here
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getRandomItem(arr) {
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    // get random item
+    return (item = arr[randomIndex]);
+  }
+
+  function generateExcuse() {
+    let whoRandom = getRandomItem(who);
+    let actionRandom = getRandomItem(action);
+    let whatRandom = getRandomItem(what);
+    let whenRandom = getRandomItem(when);
+    let excuse =
+      whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+    //alert excuse;
+    document.getElementById("RandomTextHere").innerHTML = excuse;
+  }
 };
