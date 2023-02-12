9b9cf13edffdefd61c63ce6a53289c686bd5b7dc
Marione Fiszman <marionef1985@gmail.com>
2023-02-12 13:12:43 +0000

    subir mi proyecto

---
+++ src/script.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+//write your code here
+window.onload = function() {
+  
+};
